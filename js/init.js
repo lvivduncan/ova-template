f6df83a212d8af948978672918ace71f8682b5af
lvivduncan <lvivduncan@gmail.com>
2021-10-11 16:13:58 +0300

    home mobile finish

--- js/init.js
+++ js/init.js
@@ -1,23 +1,43 @@
 // 9-10-2021
+{
+    const nav = document.querySelectorAll('#nav > ul > li');
+    for(let i = 0; i < nav.length; i++){
 
-const nav = document.querySelectorAll('#nav > ul > li');
-for(let i = 0; i < nav.length; i++){
+        // навели мишку
+        nav[i].addEventListener('mouseover', function() {
 
-    // навели мишку
-    nav[i].addEventListener('mouseover', function() {
+            this.classList.add('active');
+        });
 
-        this.classList.add('active');
-    });
+        // забрали мишку
+        nav[i].addEventListener('mouseout', function() {
 
-    // забрали мишку
-    nav[i].addEventListener('mouseout', function() {
-
-        this.classList.remove('active');
-    });
+            this.classList.remove('active');
+        });
+    }
 }
+
+// news change
+{
+
+}
+
+
+
+
+
+
+
+
 
 
 // check devices
 function isTouchDevice() {
     return (('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0));
 }
+
+
+
+
+
+// 11-10-2021
